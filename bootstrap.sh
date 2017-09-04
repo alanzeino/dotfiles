36989dee4094dc01ee4376587731519ccfc77954
Alan Zeino <alanz@uber.com>
2017-09-04 11:30:34 -0700

    reminder

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,7 +18,7 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read -p "This may overwrite existing files in your home directory (Make sure to not override your .gitconfig, Alan!). Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
